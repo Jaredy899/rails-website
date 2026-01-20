94cbe50a3e994b2c2c14916f6d0ea87043649d57
Jared Cervantes <jared@jaredcervantes.com>
2026-01-19 22:44:25 -0500

    docker fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,11 +54,12 @@ Rails.application.configure do
   config.i18n.fallbacks = true
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
+  config.hosts = [
+    "rails.jaredcervantes.com",
+    "localhost",
+    "10.24.24.6"
+  ]
+
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
